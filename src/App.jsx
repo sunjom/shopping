465465c918cfa6e0edf230cd61d6b814e6d7c4f8
Sun <saysaysay223@naver.com>
2024-07-10 01:54:19 +0900

    [FE] feat : Prop Drilling 막기 2번째
    createContext, useContext 사용

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 import Product from './components/Product.jsx';
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -66,8 +67,13 @@ function App() {
     });
   }
 
+  const ctxValue ={
+    items:shoppingCart.items,
+    addItemToCart:handleAddItemToCart
+  }
   return (
-    <>
+    //Provider를 사용하기 위해선 value값을 줘야함.
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -78,10 +84,10 @@ function App() {
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
             <Product {...product} onAddToCart={handleAddItemToCart} />
-          </li>
+          </li> 
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
